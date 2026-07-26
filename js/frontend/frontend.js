3fcf93bfc202c1f6eb2250be69232844464cc3de
Joe Plummer <jmplumm97@gmail.com>
2026-07-26 17:06:20 -0400

    changed test.html to card.html, which will be the page that has the cards that the user swipes on, also made restaurantInfo.js to hold the data structure of a restaurants information

--- js/frontend/frontend.js
+++ js/frontend/frontend.js
@@ -1,46 +1,22 @@
 $(function() {
 
     function generateCard() {
-        // declaring variables and setting attributes
-        let $card_container = $("<div></div>");
-        let $card = $("<div></div>");
-        let $card_header = $("<div></div>");
-        let $card_body = $("<div></div>");
-        $card_container.addClass("container my-3");
-        $card.addClass("card");
-        $card_header.addClass("card-header");
-        $card_body.addClass("card-body");
-        let $card_title = $("<h4></h4>");
-        let $img = $("<img>")
-        let $card_text_cuisine = $("<p></p>");
-        let $card_text_hours = $("<p></p>");
-        let $card_text_address = $("<p></p>");
-        let $card_text_distance = $("<p></p>");
-        $card_title.addClass("card-title");
-        $card_title.html("Restuarant name");
-        $img.addClass("card-img");
-        $img.attr("src", "data/panera.jpeg");
-        $img.attr("alt", "Card image");
-        $card_text_cuisine.addClass("card-text");
-        $card_text_hours.addClass("card-text");
-        $card_text_address.addClass("card-text");
-        $card_text_distance.addClass("card-text");
-        $card_text_cuisine.html("Cuisine");
-        $card_text_hours.html("Hours");
-        $card_text_address.html("Address");
-        $card_text_distance.html("Distance");
+        let $card_container = $('<div class="container my-3"><div class="card"><div class="card-header">'+
+            '<h4 class="card-title">'+
+            RestaurantInfo["Name"]+
+            '</h4></div><div class="card-body"><img class="card-img" '+
+            'src='+RestaurantInfo["image"]+
+            ' alt="Card image"><p class="card-text">'+
+            RestaurantInfo["Cuisine"]+
+            '</p><p class="card-text">'+
+            RestaurantInfo["Hours"]+
+            '</p><p class="card-text">'+
+            RestaurantInfo["Address"]+
+            '</p><p class="card-text">'+
+            RestaurantInfo["Distance"]+
+            '</p></div> </div></div>');
 
-        // putting them in order
         $("body").append($card_container);
-        $card_container.append($card);
-        $card.append($card_header);
-        $card.append($card_body);
-        $card_header.append($card_title);
-        $card_body.append($img);
-        $card_body.append($card_text_cuisine);
-        $card_body.append($card_text_hours);
-        $card_body.append($card_text_address);
-        $card_body.append($card_text_distance);
     }
 
     generateCard();
---
+++ js/frontend/restaurantInfo.js
@@ -0,0 +1,9 @@
+// To be used to store the information gathered about restaurants
+
+let RestaurantInfo = [];
+RestaurantInfo["Name"] = "Restaurant Name";
+RestaurantInfo["image"] = "data/panera.jpeg";
+RestaurantInfo["Cuisine"] = "Cuisine";
+RestaurantInfo["Hours"] = "Hours";
+RestaurantInfo["Address"] = "Address";
+RestaurantInfo["Distance"] = "Distance";

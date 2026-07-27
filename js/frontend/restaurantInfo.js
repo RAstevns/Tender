06922503a26eeb21c0902e3184de2a85c45c33b3
hilaryjaen <hilary.hjr@gmail.com>
2026-07-27 18:10:36 +0200

    Create restaurantInfo.js

---
+++ js/frontend/restaurantInfo.js
@@ -0,0 +1,28 @@
+// To be used to store the information gathered about restaurants
+// I created these 3 just to see if the swipping animation works
+let RestaurantInfo = [
+    {
+        Name: "Panera Bread",
+        image: "data/panera.jpeg",
+        Cuisine: "Cafe / Bakery",
+        Hours: "8:00 AM - 9:00 PM",
+        Address: "Campus Center",
+        Distance: "1.2 miles away"
+    },
+    {
+        Name: "McDonald's",
+        image: "data/panera.jpeg",
+        Cuisine: "American, Fast Food",
+        Hours: "7AM - 9PM",
+        Address: "123 Main Street",
+        Distance: "1.5 miles away"
+    },
+    {
+        Name: "Applebee's",
+        image: "data/panera.jpeg",
+        Cuisine: "American, Casual dining",
+        Hours: "10:30 AM - 10:00 PM",
+        Address: "250 Oak Ave",
+        Distance: "2.0 miles away"
+    }
+];

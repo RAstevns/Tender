b06b7d37fb3149825a774dc7e84d6a26ff4f6fbf
RAstevens <riley_stevens@student.uml.edu>
2026-07-27 15:09:03 -0400

    card styling update

--- js/frontend/restaurantInfo.js
+++ js/frontend/restaurantInfo.js
@@ -1,9 +1,9 @@
 // To be used to store the information gathered about restaurants
 
 let RestaurantInfo = [];
-RestaurantInfo["Name"] = "Restaurant Name";
+RestaurantInfo["Name"] = "Panera Bread";
 RestaurantInfo["image"] = "data/panera.jpeg";
-RestaurantInfo["Cuisine"] = "Cuisine";
-RestaurantInfo["Hours"] = "Hours";
-RestaurantInfo["Address"] = "Address";
-RestaurantInfo["Distance"] = "Distance";
+RestaurantInfo["Cuisine"] = "Sandwiches";
+RestaurantInfo["Hours"] = "5am-9pm";
+RestaurantInfo["Address"] = "111 Pawtucket St";
+RestaurantInfo["Distance"] = "1.1 Miles";

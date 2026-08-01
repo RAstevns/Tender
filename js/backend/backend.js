af4bf4c8e1ca3d5b6c7fc8872f755951c9ff5a61
RAstevens <riley_stevens@student.uml.edu>
2026-08-01 15:07:45 -0400

    merge fixes

--- js/backend/backend.js
+++ js/backend/backend.js
@@ -19,35 +19,20 @@ function success(position) {
 function error() {
   alert("Sorry, no position available.");
 }
+
+
 // end of code snippet
 
-//data=" + encodeURIComponent(node(around:500, 70, 2);out body;
-
-//xml version
-/*
-`<osm-script><query type="node">
-  <has-kv k="name" v="Bristol"/>
-</query>
-<around radius="10" />
-<print/></osm-script>`
-*/
-//42.6558344, -71.320074
-//[out:json];node(around:100.0,42.6558344,-71.320074);out body;
 async function searchNearby(latitude, longitude) {
   let query = `[out:json];node['amenity'='restaurant'](around:5000.0,${latitude},${longitude});
 out geom;`;
 
   $.ajax({
-//  dataType: 'text/html',
-//  contentType: 'application/json',
     type: 'GET',
     url: "https://overpass-api.de/api/interpreter",
     data: { data: query },
-    /*headers: {
-      'Access-Control-Allow-Origin' : 'https://overpass-api.de/'
-    },*/
     success: function (result) {
-      console.log(JSON.stringify(result, null, 2));
+      console.log(JSON.stringify(result, null));
     },
   });
 
